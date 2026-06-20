fe95af31e3f35b483ea25ed4ed925d6e7ad8f3ce
mjacquot82-svg <mjacquot82@gmail.com>
2026-06-20 21:15:16 +0000

    Render restaurant app from active entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
